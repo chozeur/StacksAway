9984052530d52049d9f9f1063fe9a3c574dbf382
souvikrajsingh <souvikrajsingh@gmail.com>
2024-04-03 21:03:54 +0530

    fixed-font-error

---
+++ src/components/cardData.js
@@ -0,0 +1,93 @@
+
+const cardData = [
+
+    {
+        month: "January",
+        title: "Major League Hacking",
+        program: "Mentorships",
+        picUrl: "./assets/mlh-logo-color-white.png",
+    },
+    {
+        month: "January",
+        title: "Microsoft Learn Student Ambassadors",
+        program: "Student Programs",
+        picUrl: "./assets/mlsa.jpg",
+    },
+    {
+        month: "January",
+        title: "Github Campus Expert",
+        program: "Student Programs",
+        picUrl: "./assets/gce.png",
+    },
+    {
+        month: "February",
+        title: "Google Season of Docs",
+        program: "Open Source Programs",
+        picUrl: "./assets/gsod.png",
+    },
+    {
+        month: "March",
+        title: "Google Summer of Code",
+        program: "Open Source Programs",
+        picUrl: "./assets/gsoc.png",
+    },
+
+    {
+        month: "October",
+        title: "Hacktoberfest",
+        program: "Open Source Programs",
+        picUrl: "./assets/hacktoberfest.png",
+    },
+
+    {
+        month: "January",
+        title: "Outreachy",
+        program: "Open Source Programs",
+        picUrl: "./assets/outreachy.png",
+    },
+    {
+        month: "August",
+        title: "Season of KDE",
+        program: "Open Source Programs",
+        picUrl: "./assets/kde.png",
+    },
+    {
+        month: "February",
+        title: "Open Mainframe Project Mentorship Program",
+        program: "Mentorships",
+        picUrl: "./assets/omp.png",
+    },
+    {
+        month: "September",
+        title: "FOSSASIA Codeheat",
+        program: "Open Source Programs",
+        picUrl: "./assets/fossasia.png",
+    },
+
+    {
+        month: "September",
+        title: "Redox OS Summer of Code",
+        program: "Open Source Programs",
+        picUrl: "./assets/r.png",
+    },
+    {
+        month: "December",
+        title: "Hyperledger Mentorship Program",
+        program: "Mentorships",
+        picUrl: "./assets/hmp.png",
+    },
+    {
+        month: "April",
+        title: "Open Summer of Code",
+        program: "Open Source Programs",
+        picUrl: "./assets/oso.png",
+    },
+    {
+        month: "April",
+        title: "GirlScript Summer of Code (GSSoC)",
+        program: "Open Source Programs",
+        picUrl: "./assets/gssoc.png",
+    },
+];
+
+export default cardData;
