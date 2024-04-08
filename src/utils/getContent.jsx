0ff7b27e398e4179a7603de223ad5ae626202308
souvikrajsingh <souvikrajsingh@gmail.com>
2024-04-08 22:14:09 +0530

    added-comment

--- src/utils/getContent.jsx
+++ src/utils/getContent.jsx
@@ -9,10 +9,17 @@ const getContent = () => {
 
   const getOpporunities = async () => {
     try {
+      /* Fetch entries from Contentful that have the content type 'opportunities'
+       Only select the 'fields' property of each entry */
+
       const entries = await client.getEntries({
         content_type: "opportunities",
         select: "fields",
       });
+
+      /* Map over the items in the entries
+       For each item, extract the title, description, program, month, and image URL from the fields
+       Return a new object with these properties */
 
       const sanitizedEntries = entries.items.map((items) => {
         const title = items.fields.title;
@@ -20,8 +27,9 @@ const getContent = () => {
         const program = items.fields.program;
         const month = items.fields.month;
         const image = items.fields.image.fields.file.url;
+        const slug = items.fields.slug;
 
-        return { title, description, program, month, image };
+        return { title, description, program, month, image, slug };
       });
 
       return sanitizedEntries;
