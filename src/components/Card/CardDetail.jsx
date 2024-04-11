a2f56a6464b8ec1219457c4c8af67c8788736856
souvikrajsingh <souvikrajsingh@gmail.com>
2024-04-11 23:50:31 +0530

    fixed-notFound-for-invalid-link

--- src/components/Card/CardDetail.jsx
+++ src/components/Card/CardDetail.jsx
@@ -7,6 +7,7 @@ import Navbar from "../Navbar";
 import Button from "../Buttons";
 import Footer from "../Footer";
 import { IoArrowBack } from "react-icons/io5";
+import NotFound from "../404/NotFound";
 
 const Badge = memo(({ text }) => (
   <div className="flex items-center justify-center px-3 py-2 bg-primary rounded-full text-super-dark-gray font-bold text-xs xl:text-sm 2xl:text-base">
@@ -29,18 +30,23 @@ function CardDetail() {
       setContent(response);
       const matchedCard = response.find((card) => card.slug === cardSlug);
       setCard(matchedCard);
+      setIsLoading(false);
     });
     window.scrollTo(0, 0);
   }, [cardSlug]);
 
-  if (!card) {
+  if (isLoading) {
     return null;
+  }
+
+  if (!card) {
+    return <NotFound />;
   }
 
   return (
     <>
       <Navbar />
-      <div className="grid grid-cols-1 px-6 md:grid-cols-2 gap-4 justify-items-center items-center ">
+      <div className="grid grid-cols-1 font-cabinG px-6 md:grid-cols-2 gap-4 justify-items-center items-center ">
         <Link
           to="/"
           className="font-semibold text-sm sm:text-base absolute flex top-0 left-0 text-gradient2 m-2 pt-24 sm:m-4 sm:pl-16 xl:pt-28 xl:pl-20 2xl:pt-32 2xl:pl-28 hover:text-outline transition-colors duration-150 ease-in"
@@ -71,23 +77,27 @@ function CardDetail() {
 
         {/* right side */}
 
-        <div className="pr-10 space-y-8 justify-items-center ">
-          <h1 className="text-2xl font-bold text-center mt-4 max-w-[30w] sm:text-3xl sm:font-bold sm:mt-10 2xl:text-5xl ">
+        <div className="pr-10 space-y-8 justify-items-center font-cabinG ">
+          <h1 className="text-xl ml-6 font-bold text-center mt-4 max-w-[30w] sm:text-3xl sm:font-bold sm:mt-10 2xl:text-5xl ">
             {card.title}
           </h1>
-          <p className="text-center text-base sm:text-left sm:pl-4 sm:text-lg lg:p-0 2xl:text-xl 2xl:p-0 ">
+          <p className=" pl-10 text-base sm:text-left sm:pl-4 sm:text-lg lg:p-0 2xl:text-xl 2xl:p-0 ">
             {card.description}
           </p>
           <Button href={card.link}>Visit Official Website</Button>
           <div>
             <div className=" gap-x-1 grid grid-cols-12 border-t-2 border-outline border-opacity-20 py-2">
-              <h2 className=" font-semibold col-span-4">Program</h2>
-              <span className=" col-span-8 text-text px-1">{card.program}</span>
+              <h2 className=" font-semibold col-span-4 2xl:text-lg">Program</h2>
+              <span className=" col-span-8 text-text px-1 2xl:text-lg">
+                {card.program}
+              </span>
             </div>
             <div className=" gap-x-1 grid border-t-2 border-t-dim-gray grid-cols-12 ">
-              <h2 className=" font-semibold col-span-4 pt-2">Month</h2>
+              <h2 className=" font-semibold col-span-4 pt-2 2xl:text-lg">
+                Month
+              </h2>
               <span className="flex flex-col col-span-8  text-text">
-                <span className=" py-2 border-b-2 border-outline border-opacity-20 px-1">
+                <span className=" py-2 border-b-2 border-outline border-opacity-20 px-1 2xl:text-lg">
                   {card.month}
                 </span>
                 <span className="flex space-x-6 py-2 border-b-2 border-outline border-opacity-20 px-1">
